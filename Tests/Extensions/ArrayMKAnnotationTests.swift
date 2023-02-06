0b96a593b118e75ec199462d725f1b94f9b4f279
Mikhail Vospennikov <m.vospennikov@gmail.com>
2023-02-06 11:53:34 +0400

    Add unit tests for Array+MKAnnotation extension

---
+++ Tests/Extensions/ArrayMKAnnotationTests.swift
@@ -0,0 +1,74 @@
+//
+//  ArrayMKAnnotationTests.swift
+//  
+//
+//  Created by Mikhail Vospennikov on 06.02.2023.
+//
+
+import Foundation
+import XCTest
+import CoreLocation
+import MapKit
+@testable import Cluster
+
+final class ArrayMKAnnotationTests: XCTestCase {
+    func test_subtracted() {
+        let annotation1 = Annotation(coordinate: CLLocationCoordinate2D(latitude: 37.1, longitude: -122.1))
+        let annotation2 = Annotation(coordinate: CLLocationCoordinate2D(latitude: 37.2, longitude: -122.2))
+        let annotation3 = Annotation(coordinate: CLLocationCoordinate2D(latitude: 37.3, longitude: -122.3))
+        
+        let annotations = [annotation1, annotation2, annotation3]
+        let otherAnnotations = [annotation1, annotation3]
+        let expectedResult = [annotation2]
+        
+        let result = annotations.subtracted(otherAnnotations)
+        
+        XCTAssertEqual(result, expectedResult)
+    }
+    
+    func test_subtract() {
+        let annotation1 = Annotation(coordinate: CLLocationCoordinate2D(latitude: 37.1, longitude: -122.1))
+        let annotation2 = Annotation(coordinate: CLLocationCoordinate2D(latitude: 37.2, longitude: -122.2))
+        let annotation3 = Annotation(coordinate: CLLocationCoordinate2D(latitude: 37.3, longitude: -122.3))
+        
+        var annotations = [annotation1, annotation2, annotation3]
+        let otherAnnotations = [annotation1, annotation3]
+        let expectedResult = [annotation2]
+        
+        annotations.subtract(otherAnnotations)
+        
+        XCTAssertEqual(annotations, expectedResult)
+    }
+    
+    func test_add() {
+        let annotation1 = Annotation(coordinate: CLLocationCoordinate2D(latitude: 37.1, longitude: -122.1))
+        let annotation2 = Annotation(coordinate: CLLocationCoordinate2D(latitude: 37.2, longitude: -122.2))
+        let annotation3 = Annotation(coordinate: CLLocationCoordinate2D(latitude: 37.3, longitude: -122.3))
+        let annotation4 = Annotation(coordinate: CLLocationCoordinate2D(latitude: 37.4, longitude: -122.4))
+        
+        var annotations = [annotation1, annotation2]
+        let otherAnnotations = [annotation3, annotation4]
+        
+        let expectedResult = [annotation1, annotation2, annotation3, annotation4]
+        
+        annotations.add(otherAnnotations)
+        
+        XCTAssertEqual(annotations, expectedResult)
+    }
+    
+    func test_remove() throws {
+        let annotation1 = Annotation(coordinate: CLLocationCoordinate2D(latitude: 37.1, longitude: -122.1))
+        let annotation2 = Annotation(coordinate: CLLocationCoordinate2D(latitude: 37.2, longitude: -122.2))
+        
+        var annotations = [annotation1, annotation2]
+
+        XCTAssertEqual(annotation2, try XCTUnwrap(annotations.remove(annotation2)))
+        XCTAssertEqual(annotations, [annotation1])
+        
+        XCTAssertEqual(annotation1, try XCTUnwrap(annotations.remove(annotation1)))
+        XCTAssertEqual(annotations, [])
+        
+        XCTAssertEqual(nil, annotations.remove(annotation1))
+        XCTAssertEqual(annotations, [])
+    }
+}
