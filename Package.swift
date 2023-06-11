2617e825fcf3b91b5a59a93f5fde1d248c5775c8
Mikhail Vospennikov <m.vospennikov@gmail.com>
2023-06-11 11:12:01 +0400

    Remove unsafe swift compiler flags from spm config

--- Package.swift
+++ Package.swift
@@ -15,18 +15,19 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.2.0")
     ],
     targets: [
-        .target(
-            name: "ClusterMap",
-            swiftSettings: [
-                .unsafeFlags(
-                    [
-                        "-Xfrontend", "-warn-long-expression-type-checking=100",
-                        "-Xfrontend", "-warn-long-function-bodies=100"
-                    ],
-                    .when(configuration: .debug)
-                )
-            ]
-        ),
+        .target(name: "ClusterMap"),
         .testTarget(name: "ClusterMapTests", dependencies: ["ClusterMap"])
     ]
 )
+
+//for target in package.targets {
+//    target.swiftSettings = target.swiftSettings ?? []
+//    target.swiftSettings?.append(
+//        .unsafeFlags([
+//            "-Xfrontend", "-warn-long-expression-type-checking=100",
+//            "-Xfrontend", "-warn-long-function-bodies=100",
+//            "-enable-library-evolution",
+//            "-enable-testing"
+//        ])
+//    )
+//}
