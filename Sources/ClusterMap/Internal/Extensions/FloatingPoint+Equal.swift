53f830e2766598923669bf4af6c1dfcbed16731e
Mikhail Vospennikov <m.vospennikov@gmail.com>
2023-12-28 09:49:26 +0400

    Reduce function compile complexity

--- Sources/ClusterMap/Internal/Extensions/FloatingPoint+Equal.swift
+++ Sources/ClusterMap/Internal/Extensions/FloatingPoint+Equal.swift
@@ -9,16 +9,27 @@ import Foundation
 
 extension FloatingPoint {
     func isNearlyEqual(to value: Self) -> Bool {
+        if self == value {
+            return true
+        }
+        
         let absA = abs(self)
         let absB = abs(value)
         let diff = abs(self - value)
-
-        if self == value {
-            return true
-        } else if self == .zero || value == .zero || (absA + absB) < .leastNormalMagnitude {
-            return diff < .ulpOfOne * .leastNormalMagnitude
+        
+        if self == .zero || value == .zero || (absA + absB) < .leastNormalMagnitude {
+            return isDiffLessThanUlp(diff)
         } else {
-            return diff / min(absA + absB, .greatestFiniteMagnitude) < .ulpOfOne
+            return isDiffSmallEnough(diff, absA, absB)
         }
+    }
+    
+    private func isDiffLessThanUlp(_ diff: Self) -> Bool {
+        return diff < .ulpOfOne * .leastNormalMagnitude
+    }
+    
+    private func isDiffSmallEnough(_ diff: Self, _ absA: Self, _ absB: Self) -> Bool {
+        let sumMin = min(absA + absB, .greatestFiniteMagnitude)
+        return diff / sumMin < .ulpOfOne
     }
 }
