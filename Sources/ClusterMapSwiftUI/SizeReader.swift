9bcd463ef856f8fb289820c86959c1741c16f3f8
Adam Cichy <adam@moonlightapps.com>
2024-11-06 12:10:58 +0100

    Make the SizePreferenceKey.defaultValue a constant
    Changing this to a constant will make it compatible with Swift 6. 
    
    Global mutable variables are not concurrency safe and Swift 6 treats it as an error

--- Sources/ClusterMapSwiftUI/SizeReader.swift
+++ Sources/ClusterMapSwiftUI/SizeReader.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct SizePreferenceKey: PreferenceKey {
-    static var defaultValue: CGSize = .zero
+    static let defaultValue: CGSize = .zero
     static func reduce(value: inout CGSize, nextValue: () -> CGSize) { }
 }
 
